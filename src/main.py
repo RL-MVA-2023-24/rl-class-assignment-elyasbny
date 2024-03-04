2514135410df546d3ae5eae9282b2d304b4bed43
elyasbny <elyasbetter@gmail.com>
2024-03-04 20:59:24 +0100

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -3,9 +3,8 @@ import os
 import numpy as np
 import torch
 
-from evaluate import evaluate_HIV, evaluate_HIV_population
-from train import ProjectAgent  # Replace DummyAgent with your agent implementation
-
+from evaluate import evaluate_HIV, evaluate_HIV_population  
+from train import ProjectAgent # Replace DummyAgent with your agent implementation
 
 def seed_everything(seed: int = 42):
     random.seed(seed)
